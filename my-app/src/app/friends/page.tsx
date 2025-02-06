659377ab11ff30f03ec82372183aa42295f652ef
scorpion963 <jackson20062008@gmail.com>
2025-02-05 21:19:40 -0600

    Starting the development of the game functinoality

--- my-app/src/app/friends/page.tsx
+++ my-app/src/app/friends/page.tsx
@@ -1,7 +1,8 @@
 export default async function FriendsPage() {
   return (
     <div className="flex items-center justify-center h-screen bg-foreground">
-      
+      <div className="border border-opacity-10 w-4/5 h-4/5 rounded-lg">
+      </div>
     </div>
   );
 }
--- my-app/src/app/game/page.tsx
+++ my-app/src/app/game/page.tsx
@@ -1,20 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function GamePage() {
   return (
     <div className="w-full h-screen text-background flex items-center flex-col gap-12 justify-center bg-foreground">
       <div className="flex flex-col items-center justify-center ">
         <h1 className="text-4xl  font-bold text-center">Game Page</h1>
       </div>
-      <div className="w-[800px] h-[800px] border border-blue-100">
-        <div className="grid grid-cols-3 grid-rows-3 w-full h-full border border-white">
-          <div className="border border-white"></div>
-          <div className="border border-white"></div>
-          <div className="border border-white"></div>
-          <div className="border border-white"></div>
-          <div className="border border-white"></div>
-          <div className="border border-white"></div>
-          <div className="border border-white"></div>
-          <div className="border border-white"></div>
-          <div className="border border-white"></div>
+      <div className="w-[800px] h-[800px] border border-blue-100 border-opacity-10">
+        <div className="grid grid-cols-3 grid-rows-3 w-full h-full">
+          {Array.from({ length: 9 }, (_, i) => i + 1).map((i) => <Button className="w-full h-full rounded-none text-4xl" key={i}></Button>)}
         </div>
       </div>
     </div>
